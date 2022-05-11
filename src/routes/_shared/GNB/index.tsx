1d0913567e797b208957e5dd11621c8438ce65ae
jeonhaekang <73621658+jeonhaekang@users.noreply.github.com>
2022-05-11 11:00:54 +0900

    feat : GNB 생성

---
+++ src/routes/_shared/GNB/index.tsx
@@ -0,0 +1,24 @@
+import { Link, NavLink } from 'react-router-dom'
+import cx from 'classnames'
+import styles from './GNB.module.scss'
+
+const GNB = () => {
+  return (
+    <nav className={styles.gnb}>
+      <ul>
+        <li>
+          <NavLink to='/' className={({ isActive }) => cx({ [styles.isActive]: isActive })}>
+            main
+          </NavLink>
+        </li>
+        <li>
+          <NavLink to='/bookmark' className={({ isActive }) => cx({ [styles.isActive]: isActive })}>
+            bookmark
+          </NavLink>
+        </li>
+      </ul>
+    </nav>
+  )
+}
+
+export default GNB
