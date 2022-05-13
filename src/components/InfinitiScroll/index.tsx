ca3057e5a8d551c7f1d9726d4128ec8031cf6636
jeonhaekang <73621658+jeonhaekang@users.noreply.github.com>
2022-05-13 18:35:52 +0900

    refactor : axios 통신 promise -> async await으로 변경

--- src/components/InfinitiScroll/index.tsx
+++ src/components/InfinitiScroll/index.tsx
@@ -2,53 +2,55 @@ import styles from './InfinityScroll.module.scss'
 import { ReactNode, useEffect, useMemo, useRef, useState } from 'react'
 
 import { getMovieList } from 'axios/movieApi'
-import { AxiosResponse } from 'axios'
 
 import { useRecoilState, useSetRecoilState } from 'recoil'
 import { movieListState } from 'state/movie'
 import { modalState } from 'state/modal'
 
 import { Spinner } from '../../assets/svg/index'
+import { ISearchResult } from 'types/Movie'
 
 interface Props {
   children: ReactNode
 }
 
 const InfinityScroll = ({ children }: Props) => {
-  const [movieList, setMovieList] = useRecoilState(movieListState)
+  const [movieList, setMovieList] = useRecoilState<ISearchResult>(movieListState)
   const [isNext, setIsNext] = useState(false)
   const spinnerRef = useRef<HTMLDivElement>(null)
   const setModalState = useSetRecoilState(modalState)
 
-  const observer = useMemo(
+  const callBackHandler: IntersectionObserverCallback = useMemo(
     () =>
-      new IntersectionObserver(async ([{ isIntersecting, target }]) => {
+      async ([{ isIntersecting, target }], observer) => {
         if (!isIntersecting) {
           return
         }
+
         observer.unobserve(target)
 
-        getMovieList({ s: movieList.s, page: movieList.page + 1 })
-          .then((res: AxiosResponse) => {
-            setMovieList((prev) => {
-              return {
-                ...prev,
-                page: prev.page + 1,
-                movieList: [...prev.movieList, ...res.data.Search],
-                totalResults: prev.totalResults - 10,
-              }
-            })
+        try {
+          const result = await getMovieList({ s: movieList.s, page: movieList.page + 1 })
+          setMovieList((prev) => {
+            return {
+              ...prev,
+              page: prev.page + 1,
+              movieList: [...prev.movieList, ...result.data.Search],
+              totalResults: prev.totalResults - 10,
+            }
+          })
+        } catch (err) {
+          setModalState((prev) => {
+            return { ...prev, state: true, message: '알 수 없는 에러가 발생하였습니다.' }
+          })
+        }
 
-            observer.observe(target)
-          })
-          .catch((err) => {
-            setModalState((prev) => {
-              return { ...prev, state: true, message: err.message }
-            })
-          })
-      }),
+        observer.observe(target)
+      },
     [movieList.page, movieList.s, setModalState, setMovieList]
   )
+
+  const observer = useMemo(() => new IntersectionObserver(callBackHandler), [callBackHandler])
 
   useEffect(() => {
     if (spinnerRef.current) observer.observe(spinnerRef.current)
@@ -57,11 +59,7 @@ const InfinityScroll = ({ children }: Props) => {
   }, [observer, isNext])
 
   useEffect(() => {
-    if (movieList.totalResults >= 10) {
-      setIsNext(true)
-    } else {
-      setIsNext(false)
-    }
+    setIsNext(movieList.totalResults >= 10)
   }, [movieList])
 
   return (
