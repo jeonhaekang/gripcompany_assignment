36f278cb57e6689f499125d10ef5762bd31e9b32
jeonhaekang <73621658+jeonhaekang@users.noreply.github.com>
2022-05-13 17:34:09 +0900

    feat : 최초 로딩시 로컬스토리지에서 북마크 가져와 업데이트

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -1,13 +1,29 @@
 import styles from './Routes.module.scss'
 import { Routes, Route } from 'react-router-dom'
+import { useMount } from 'react-use'
+
+import { useSetRecoilState } from 'recoil'
+import { bookMarkList } from 'state/movie'
+
+import { IMovieItem } from 'types/Movie'
+
+import { getBookmark } from 'utils/localStorage'
 
 import Main from './Main'
 import Bookmark from './Bookmark'
 import GNB from './_shared/GNB'
+import Modal from 'components/Modal'
 
 const App = () => {
+  const initialBookmark = useSetRecoilState<IMovieItem[]>(bookMarkList)
+
+  useMount(() => {
+    initialBookmark(getBookmark())
+  })
+
   return (
     <div className={styles.mobileFrame}>
+      <Modal />
       <div className={styles.appWrapper}>
         <Routes>
           <Route path='/' element={<Main />} />
