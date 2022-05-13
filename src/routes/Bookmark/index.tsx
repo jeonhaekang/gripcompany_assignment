cf28846693ed1e7980369315f7f33ee8f2af1c7b
jeonhaekang <73621658+jeonhaekang@users.noreply.github.com>
2022-05-13 17:33:32 +0900

    feat : 북마크 페이지 추가

--- src/routes/Bookmark/index.tsx
+++ src/routes/Bookmark/index.tsx
@@ -1,5 +1,25 @@
+import styles from './Bookmark.module.scss'
+
+import { useRecoilValue } from 'recoil'
+import { bookMarkList } from 'state/movie'
+
+import { IMovieItem } from 'types/Movie'
+
+import Card from 'components/Card'
+
 const Bookmark = () => {
-  return <div>Bookmark</div>
+  const list = useRecoilValue<IMovieItem[]>(bookMarkList)
+
+  return (
+    <div>
+      <h1>내 즐겨찾기</h1>
+      <ul className={styles.movieList}>
+        {list.map((item) => {
+          return <Card key={item.imdbID} data={item} />
+        })}
+      </ul>
+    </div>
+  )
 }
 
 export default Bookmark
