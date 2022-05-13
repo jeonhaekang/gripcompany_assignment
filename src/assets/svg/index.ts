56515171bbdc983082a4fbdbe4c561d8980adaf0
jeonhaekang <73621658+jeonhaekang@users.noreply.github.com>
2022-05-13 17:30:28 +0900

    feat : 북마크 등록 기능 추가

--- src/assets/svg/index.ts
+++ src/assets/svg/index.ts
@@ -1,4 +1,5 @@
 import { ReactComponent as SearchIcon } from './search.svg'
 import { ReactComponent as Spinner } from './spinner.svg'
+import { ReactComponent as BookmarkIcon } from './bookmark.svg'
 
-export { SearchIcon, Spinner }
+export { SearchIcon, Spinner, BookmarkIcon }
---
+++ src/components/Card/index.tsx
@@ -0,0 +1,70 @@
+import styles from './Card.module.scss'
+import { memo, SyntheticEvent, useEffect, useState } from 'react'
+
+import { useRecoilState, useSetRecoilState } from 'recoil'
+import { modalState } from 'state/modal'
+import { bookMarkList } from 'state/movie'
+
+import { IMovieItem } from 'types/Movie'
+
+import { addBookmark, delBookmark, isBookmarked } from 'utils/localStorage'
+
+import emptyImage from '../../assets/Image/emptyImage.png'
+import { BookmarkIcon } from 'assets/svg'
+
+interface Props {
+  data: IMovieItem
+}
+
+const Card = ({ data }: Props) => {
+  const { Poster, Title, Year, Type, imdbID } = data
+  const [isBookmark, setIsBookmark] = useState(false)
+  const [bookmarkList, setBookmarkList] = useRecoilState(bookMarkList)
+  const setModalState = useSetRecoilState(modalState)
+
+  const imageOnErrorHandler = (e: SyntheticEvent<HTMLImageElement>) => {
+    e.currentTarget.src = emptyImage
+  }
+
+  const confirmAction = () => {
+    setBookmarkList(isBookmark ? delBookmark(imdbID) : addBookmark(data))
+  }
+
+  const confirmMessage = () => {
+    return isBookmark ? `${Title}을 북마크에서 삭제하시겠습니까?` : `${Title}을 북마크에 등록하시겠습니까?`
+  }
+
+  const onClickHandler = () => {
+    setModalState((prev) => {
+      return { ...prev, state: true, type: 'confirm', message: confirmMessage(), action: confirmAction }
+    })
+  }
+
+  useEffect(() => {
+    setIsBookmark(isBookmarked(imdbID))
+  }, [bookmarkList, imdbID])
+
+  return (
+    <li className={styles.movieItem} onClick={onClickHandler} role='presentation'>
+      {isBookmark && <BookmarkIcon />}
+      <div>
+        <img src={Poster} alt={`${Title}-poster`} onError={imageOnErrorHandler} />
+      </div>
+      <dl>
+        <dt>
+          <h1>{Title}</h1>
+        </dt>
+        <dd>
+          <ul>
+            <li className={styles.chip}>{Type.toUpperCase()}</li>
+            <li>
+              <time className={styles.chip}>{Year}</time>
+            </li>
+          </ul>
+        </dd>
+      </dl>
+    </li>
+  )
+}
+
+export default memo(Card)
