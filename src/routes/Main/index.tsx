9e12a55ebec0c9d829bac1c5d7f22672773e01bf
jeonhaekang <73621658+jeonhaekang@users.noreply.github.com>
2022-05-14 16:26:31 +0900

    refactor : 중복 영화 제거 및 lodash 설치

--- src/routes/Main/index.tsx
+++ src/routes/Main/index.tsx
@@ -5,22 +5,26 @@ import { movieListState } from 'state/movie'
 
 import { ISearchResult } from '../../types/Movie.d'
 
-import Search from './Search'
 import Card from 'components/Card'
 import InitalMessage from './InitalMessage'
 import InfinityScroll from 'components/InfinitiScroll'
+import { useMemo } from 'react'
+import _ from 'lodash'
 
 const Main = () => {
   const searchResult = useRecoilValue<ISearchResult>(movieListState)
 
+  const { movieList } = searchResult
+
+  const uniqArray = useMemo(() => _.uniqBy(movieList, 'imdbID'), [movieList])
+
   return (
     <div className={styles.main}>
-      <Search />
       <main>
-        {searchResult.s ? (
+        {movieList.length > 0 ? (
           <InfinityScroll>
             <ul className={styles.movieList}>
-              {searchResult.movieList.map((item) => {
+              {uniqArray.map((item) => {
                 return <Card key={item.imdbID} data={item} />
               })}
             </ul>
