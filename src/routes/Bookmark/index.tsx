56cd337cf3adc9e7cf5806b79d0159be8ab991a7
jeonhaekang <73621658+jeonhaekang@users.noreply.github.com>
2022-05-14 17:57:58 +0900

    feat : 북마크 비었을 때 메세지 추가

--- src/routes/Bookmark/index.tsx
+++ src/routes/Bookmark/index.tsx
@@ -6,18 +6,23 @@ import { bookMarkList } from 'state/movie'
 import { IMovieItem } from 'types/Movie'
 
 import Card from 'components/Card'
+import InitalMessage from 'components/InitalMessage'
 
 const Bookmark = () => {
   const list = useRecoilValue<IMovieItem[]>(bookMarkList)
 
   return (
-    <div>
-      <h1>내 즐겨찾기</h1>
-      <ul className={styles.movieList}>
-        {list.map((item) => {
-          return <Card key={item.imdbID} data={item} />
-        })}
-      </ul>
+    <div className={styles.bookmark}>
+      <h1 className={styles.title}>나의 북마크</h1>
+      {list.length > 0 ? (
+        <ul className={styles.movieList}>
+          {list.map((item) => {
+            return <Card key={item.imdbID} data={item} />
+          })}
+        </ul>
+      ) : (
+        <InitalMessage />
+      )}
     </div>
   )
 }
