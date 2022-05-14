13c0e998918e5b06ec0676e143b8de7ab4259bb6
jeonhaekang <73621658+jeonhaekang@users.noreply.github.com>
2022-05-14 17:57:33 +0900

    refactor : initialMessage 디렉토리 변경 및 북마크 페이지 연동

---
+++ src/components/InitalMessage/index.tsx
@@ -0,0 +1,15 @@
+import { useLocation } from 'react-use'
+import emptyResult from '../../assets/Image/emptyResult.png'
+import styles from './InitalMessage.module.scss'
+
+const InitalMessage = () => {
+  const { pathname } = useLocation()
+  return (
+    <div className={styles.enterSearch}>
+      <img src={emptyResult} alt='emptyResult' />
+      <h1>{pathname === '/' ? '검색 결과가 없습니다.' : '북마크가 비었습니다.'}</h1>
+    </div>
+  )
+}
+
+export default InitalMessage
